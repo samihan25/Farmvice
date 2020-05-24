333590915d236a051e9548edc4a33d24647c3a23
Samihan Deshmukh <samihan25@gmail.com>
2020-05-24 17:08:44 +0530

    NPK values validation and python function modularisation

--- Farmvice/views.py
+++ Farmvice/views.py
@@ -20,17 +20,31 @@ def index(request):
     return render(request, 'index.html')
 
 def predict(request):
-    nitrogen=float(request.POST["nitrogen"])
-    phosphorus=float(request.POST["phosphorus"])
-    potassium=float(request.POST["potassium"])
-    pH_value=float(request.POST["pH_value"])
-    season=request.POST["season"]
-    district=request.POST["district"]
+    nitrogen = float(request.POST["nitrogen"])
+    phosphorus = float(request.POST["phosphorus"])
+    potassium = float(request.POST["potassium"])
+    
+    district = request.POST["district"]
+    season = request.POST["season"]
+    pH_value = float(request.POST["pH_value"])
 
     #insert machine learning code here
-    
+
     #n,p,k soil quality prediction code
     #start here
+    soil_quality = soil_quality_fun(nitrogen,phosphorus,potassium)
+    #ends here
+
+    #crop prediction code
+    #starts here
+    crop = crop_predict(district,season,pH_value,soil_quality)
+    #ends here
+    
+    pesticide="wxyz"
+
+    return render(request,'predict.html',{'nitrogen':nitrogen, 'phosphorus':phosphorus, 'potassium':potassium, 'pH_value':pH_value, 'season':season, 'district':district, 'soil_quality':soil_quality, 'crop':crop, 'pesticide':pesticide})
+
+def soil_quality_fun(nitrogen,phosphorus,potassium):
     df=pd.read_csv("npk.csv")
 
     X = df.iloc[:,:-1]
@@ -39,11 +53,9 @@ def predict(request):
     clf = clf.fit(X, Y)
 
     soil_quality=clf.predict([[nitrogen,phosphorus,potassium]])
-    soil_quality=soil_quality[0]
-    #ends here
+    return soil_quality[0]
 
-    #crop prediction code
-    #starts here
+def crop_predict(district,season,pH_value,soil_quality):
     import Orange
 
     data = Orange.data.Table("crop.tab")
@@ -56,19 +68,16 @@ def predict(request):
     input_district = district
     input_season = season
     input_soil_quality = soil_quality
-    input_ph_4_45=0
-    input_ph_45_5=0
-    input_ph_5_55=0
-    input_ph_55_6=1
-    input_ph_6_65=1
-    input_ph_65_7=0
-    input_ph_7_75=0
+    input_ph_4_45 = 0
+    input_ph_45_5 = 0
+    input_ph_5_55 = 0
+    input_ph_55_6 = 1
+    input_ph_6_65 = 1
+    input_ph_65_7 = 0
+    input_ph_7_75 = 0
 
     input_data=[[input_district,input_season,input_soil_quality,input_ph_4_45,input_ph_45_5,input_ph_5_55,input_ph_55_6,input_ph_6_65,input_ph_65_7,input_ph_7_75]]
 
     crop = c_values[int(cn2_classifier(input_data))]
-    #ends here
-    
-    pesticide="wxyz"
 
-    return render(request,'predict.html',{'nitrogen':nitrogen, 'phosphorus':phosphorus, 'potassium':potassium, 'pH_value':pH_value, 'season':season, 'district':district, 'soil_quality':soil_quality, 'crop':crop, 'pesticide':pesticide})
+    return crop
